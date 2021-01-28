c317a031356cb9bce1c647466945fccabdb0c461
alejandrourbina <urbinagonzalezja@gmail.com>
2021-01-28 13:23:36 +0100

    basket and image update. and be able to print prices

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
+var dataBase = {
+    wine: 10,
+    peanut: 4.5,
+    bread: 1,
+    flour: 3,
+    milk: 2,
+    oil: 1.5,
+    ketchup: 3.2,
+    rice: 0.45,
+    water: 1,
+    chip: 2,
+}
+
+var precioTotal = 0.0;
+
 function allowDrop(ev) {
+    console.log("allow");
     ev.preventDefault();
 }
   
@@ -8,28 +24,30 @@ function drag(ev) {
 
 function drop(ev) {
     ev.preventDefault();
+
     var data = ev.dataTransfer.getData("text");
+ 
+    var tipo = data.substring(0, data.length - 1);
+
+    var precio = dataBase[tipo];
+
+    precioTotal += precio;
+
+    document.getElementById('pay').innerHTML= precioTotal.toFixed(2);
+    
+    console.log(precioTotal);
+
+    document.getElementById("total");
+
     ev.target.appendChild(document.getElementById(data));
 }
 
-var dataBase = {
-    wine: 10,
-    peanutButter: 4.5,
-    bread: 1,
-    flour: 3,
-    milk: 2,
-    oil: 1.5,
-    ketchup: 3.2,
-    salt: 0.45,
-    water: 1,
-    chips: 2,
+function remove() {
+    var last = document.getElementById("shoppingBasket").parentElement;
+    last.removeChild(document.getElementById("shoppingBasket"));
 }
 
-
-
-
-
-
-
-
-
+/*function remove () {
+    var last = document.getElementById('shoppingBasket');
+    document.body.removeChild(last.lastElementChild);
+}*/
